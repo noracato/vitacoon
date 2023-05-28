f00c456c7fa2d83f864d4b13e738ad575686d892
noracato <ncschinkel@gmail.com>
2023-05-28 15:11:00 +0200

    Use vitacolors

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let minimalTime = 3000;
 let circleGen;
 let circleTimer = 1000;
+let colorNumber = 0;
+let colors = ['#634ea0', '#f9d49b', '#8a712f', '#e7c21f', '#f89943',   '#e42432',  ]
 
 $('.container').bind('mousedown touchstart', circlePop);
 $('.container').bind('mouseup touchend', function(){
@@ -19,9 +21,16 @@ function circlePop(e) {
 function makeCircle(x, y) {
     let circle = $('<div></div>');
     circle.addClass('circle');
+    circle.css('background-color', colors[colorNumber])
     $('.container').append(circle);
 
     let size = parseInt(circle.css('height')) / 2;
     circle.css('top', y - size);
     circle.css('left', x - size);
+
+    if (colorNumber == colors.length) {
+        colorNumber  = 0;
+    } else {
+        colorNumber += 1;
+    }
 }
